70aad50afaf7aa2ba183e3223c1b5758a061e557
Tyler Frankenstein <tyler.frankenstein@gmail.com>
2013-06-09 02:24:54 -0400

    Wrapped the home page in a try catch. Adjusted models to match new hook_mvc_model() schema.

--- modules/custom/golfasaurus/golfasaurus.js
+++ modules/custom/golfasaurus/golfasaurus.js
@@ -6,8 +6,12 @@ function golfasaurus_menu() {
     var items = {
       'home':{
         'title':'Home',
-        'page_callback':'golfasaurus_home',
-      }
+        'page_callback':'golfasaurus_home_page',
+      }/*,
+      'players':{
+        'title':'Players',
+        'page_callback':'golfasaurus_players_page',
+      },*/
     };
     return items;
   }
@@ -16,20 +20,54 @@ function golfasaurus_menu() {
   }
 }
 
+
+
 /**
  * Page callback for home.
  */
-function golfasaurus_home() {
-  var score = drupalgap_mvc_model_load('golfasaurus', 'score');
-  var content = {
-    'players':{
-      'theme':'button_link',
-      'path':'local_users/list',
-      'text':'Players',
-    },
-  };
-  return content;
+function golfasaurus_home_page() {
+  try {
+    var content = {
+      'players':{
+        'theme':'button_link',
+        'path':'local_users/list',
+        'text':'Players',
+      }
+    };
+    return content;
+  }
+  catch (error) {
+    alert('golfasaurus_home_page - ' + error);
+  }
+  
 }
+
+/**
+ * Page callback for players.
+ */
+/*function golfasaurus_players_page() {
+  try {
+    var content = {
+      'players_list':{
+        'theme':'jqm_item_list',
+        'title':'Players'
+      }
+    };
+    var items = [];
+    var players = collection_load('golfasaurus', 'players');
+    if (players) {
+      $.each(players, function(uid, player){
+        items.push(l(player.name));
+      });
+      content.players_list.items = items;  
+    }
+    return content;
+  }
+  catch (error) {
+    alert('golfasaurus_home_page - ' + error);
+  }
+  
+}*/
 
 /**
  * Implements hook_mvc_model().
@@ -37,29 +75,23 @@ function golfasaurus_home() {
 function golfasaurus_mvc_model() {
   var models = {
     "score":{
-      "sid":{
-        "type":"hidden",
-        "title":"Score ID",
-        "required":true,
-        "primary_key":true
-      },
-      "course":{
-        "type":"textfield",
-        "title":"Course",
-        "required":true
+      "primary_key":"sid",
+      "fields":{
+        "course":{
+          "type":"textfield",
+          "title":"Course",
+          "required":true
+        }
       }
     },
     "course":{
-      "cid":{
-        "type":"hidden",
-        "title":"Course ID",
-        "required":true,
-        "primary_key":true
-      },
-      "location":{
-        "type":"textfield",
-        "title":"Location",
-        "required":true
+      "primary_key":"cid",
+      "fields":{
+        "location":{
+          "type":"textfield",
+          "title":"Location",
+          "required":true
+        }
       }
     }
   };
